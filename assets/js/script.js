41b243c854bf91cb84ba6659660b180e1956885e
Tobi Olajide <tobiolajide887@yahoo.com>
2023-01-27 16:13:36 +0000

    added function to renderSearchHistory and prepend city searches buttons to page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,26 @@
 
+// Function to handle click of search button
 function searchCity(event) {
+    // Prevent searh form default to save form input
     event.preventDefault();
-    console.log("Hello")
+
+    // Render search history
+    renderSearchHistory();
+    // console.log(city)
 }
 
+// Function to render search history 
+function renderSearchHistory() {
+    // store search input value i.e, name of city
+    let city = document.querySelector("#search-input").value
+
+    // Generate search history buttons: create/set content and prepend buttons to page
+    let searchedCityBtn = document.createElement("div");
+    searchedCityBtn.innerHTML = `
+                                 <button class ="row">${city}</button>
+                                `
+    document.querySelector("#history").prepend(searchedCityBtn);
+}
+
+// event listener for the button to search for city
 document.querySelector("#search-button").addEventListener("click", searchCity)
